db2acca96d7b75a2c24b05f448b466f7d6011fb3
Marek Zelinka <mzelinka17@gmail.com>
2026-01-10 16:18:13 +0100

    setup cors

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,34 @@
-from pydantic import PostgresDsn
+from typing import Annotated, Any
+
+from pydantic import AnyUrl, BeforeValidator, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
+
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
 
     database_url: PostgresDsn
     secret_key: str
     access_token_expire_minutes: int
+    cors_origins: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = []
+
+    @computed_field
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.cors_origins]
 
 
 config = Settings()  # ty:ignore[missing-argument]
--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.config import config
 from app.deps import SessionDep
 from app.routers import auth, posts, votes
 
 app = FastAPI(title="Social Media API", version="1.0.0")
+
+# Set all CORS enabled origins
+if config.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,  # ty:ignore[invalid-argument-type]
+        allow_origins=config.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 
 app.include_router(auth.router)
 app.include_router(posts.router)
