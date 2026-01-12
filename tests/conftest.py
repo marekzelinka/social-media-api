4f50cb5073fc9bda7ade72f770a7a04c4a929e61
Marek Zelinka <mzelinka17@gmail.com>
2026-01-12 10:15:50 +0100

    fix testing setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,15 +11,13 @@ from app.deps import get_session
 from app.main import app
 
 
-# Setup a single engine for the entire test session
 @pytest.fixture(scope="session")
 def engine() -> Engine:
     return create_engine(str(config.test_database_url))
 
 
-# Runs db migrations once per test session
 @pytest.fixture(scope="session", autouse=True)
-def setup_database():
+def setup_database() -> Generator[None]:
     alembic_cfg = Config("alembic.ini")
     alembic_cfg.set_main_option("sqlalchemy.url", str(config.test_database_url))
     command.upgrade(alembic_cfg, "head")
@@ -31,7 +29,7 @@ def setup_database():
 def session_fixture(engine: Engine) -> Generator[Session]:
     connection = engine.connect()
     transaction = connection.begin()
-    session = Session(bind=connection)
+    session = Session(bind=connection, join_transaction_mode="create_savepoint")
     yield session
     session.close()
     transaction.rollback()
@@ -40,6 +38,7 @@ def session_fixture(engine: Engine) -> Generator[Session]:
 
 @pytest.fixture(name="client")
 def client_fixture(session: Session) -> Generator[TestClient]:
+    # Set session ovverride before startup lifecycle events trigger
     app.dependency_overrides[get_session] = lambda: session
     with TestClient(app) as client:
         yield client
--- tests/routes/test_auth.py
+++ tests/routes/test_auth.py
@@ -4,27 +4,30 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session
 
 from app.core.security import hash_password
-from app.models import User, UserPublic
+from app.models import User, UserCreate
 
 
 @pytest.fixture
-def form_data(session: Session) -> dict[str, str]:
-    user_dict = {
-        "username": "hello123",
-        "email": "hello123@gmail.com",
-        "password": "password123",
-    }
-    hashed_password = hash_password(user_dict["password"])
+def user(session: Session) -> UserCreate:
+    user = UserCreate.model_validate(
+        {
+            "username": "hello123",
+            "email": "hello123@gmail.com",
+            "password": "password123",
+        }
+    )
+    hashed_password = hash_password(user.password)
+    user_dict = user.model_dump()
     new_user = User.model_validate(
         user_dict, update={"hashed_password": hashed_password}
     )
     session.add(new_user)
     session.commit()
-    return {"username": new_user.username, "password": user_dict["password"]}
+    return user
 
 
 def test_register(client: TestClient) -> None:
-    response = client.post(
+    r = client.post(
         "/register",
         json={
             "username": "hello123",
@@ -32,15 +35,19 @@ def test_register(client: TestClient) -> None:
             "password": "password123",
         },
     )
-    assert response.status_code == status.HTTP_201_CREATED
-    data = response.json()
-    new_user = UserPublic(**data)
-    assert new_user.email == "hello123@gmail.com"
+    assert r.status_code == status.HTTP_201_CREATED
+    data = r.json()
+    assert data["email"] == "hello123@gmail.com"
 
 
-def test_token(client: TestClient, form_data) -> None:
-    response = client.post(
+def test_token(client: TestClient, user: UserCreate) -> None:
+    r = client.post(
         "/token",
-        data=form_data,
+        data={
+            "username": user.username,
+            "password": user.password,
+        },
     )
-    assert response.status_code == status.HTTP_200_OK
+    assert r.status_code == status.HTTP_200_OK
+    data = r.json()
+    assert data["access_token"]
