1478ffe6ca497f28300cd6270a4c4eeb0749fd24
Marek Zelinka <mzelinka17@gmail.com>
2026-01-12 17:20:49 +0100

    testing create post

--- tests/routes/test_posts.py
+++ tests/routes/test_posts.py
@@ -1,9 +1,11 @@
 from collections.abc import Sequence
+from typing import Any
 
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from app.models import Post, PostPublicWithVotes
+from app.models import Post, PostPublic, PostPublicWithVotes
 
 
 def test_read_posts(
@@ -25,3 +27,69 @@ def test_read_posts(
 def test_read_post(client: TestClient, db_posts: Sequence[Post]) -> None:
     r = client.get(f"/posts/{db_posts[0].id}")
     assert r.status_code == status.HTTP_200_OK
+
+
+def test_read_post_with_nonexisting_id(client: TestClient) -> None:
+    nonexisting_id = "63df3284-94fe-42e2-be37-dfc6d38f374e"
+    r = client.get(f"/posts/{nonexisting_id}")
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_read_post_with_invalid_id(client: TestClient) -> None:
+    invalid_id = "invalid_post_id"
+    r = client.get(f"/posts/{invalid_id}")
+    assert r.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+
+@pytest.mark.parametrize(
+    "title, content, published",
+    [
+        ("awesome new title", "amesome new content", True),
+        ("favorite pizza", "i love pepperoni", False),
+        ("tallest skyscrapers", "wahoo", True),
+    ],
+)
+def test_create_post_with_valid_data(
+    client: TestClient,
+    db_user: dict[str, Any],
+    token_headers: dict[str, str],
+    title: str,
+    content: str,
+    published: bool,
+) -> None:
+    r = client.post(
+        "/posts/",
+        headers=token_headers,
+        json={"title": title, "content": content, "published": published},
+    )
+    assert r.status_code == status.HTTP_201_CREATED
+    data = r.json()
+    new_post = PostPublic.model_validate(data)
+    assert new_post.title == title
+    assert new_post.content == content
+    assert new_post.published == published
+    assert new_post.owner_id == db_user["id"]
+
+
+def test_create_post_default_published_true(
+    client: TestClient,
+    db_user: dict[str, Any],
+    token_headers: dict[str, str],
+) -> None:
+    r = client.post(
+        "/posts",
+        headers=token_headers,
+        json={"title": "arbitrary title", "content": "content"},
+    )
+    assert r.status_code == status.HTTP_201_CREATED
+    data = r.json()
+    new_post = PostPublic.model_validate(data)
+    assert new_post.title == "arbitrary title"
+    assert new_post.content == "content"
+    assert new_post.published is True
+    assert new_post.owner_id == db_user["id"]
+
+
+def test_create_post_unauthorized_without_headers(client: TestClient) -> None:
+    r = client.post("/posts/")
+    assert r.status_code == status.HTTP_401_UNAUTHORIZED
